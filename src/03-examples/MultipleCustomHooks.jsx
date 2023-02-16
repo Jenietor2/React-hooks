54b315d00d7f97eebd839de6c1cd41deec0c13ae
jenietor <jenietor0311@gmail.com>
2023-02-16 17:44:19 -0500

    se crea un nuevo custome hook

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,36 @@
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch"
 
 
 export const MultipleCustomHooks = () => {
+  
+  const { counter, increment } = useCounter(1);
+
+  const {data, isLoading, hasError} = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${counter}`);
+
+  const {quote, author} = !!data && data[0];
+
+  console.log(counter);
+
   return (
     <>
         <h1>BreakingBad Quotes</h1>
         <hr/>
+{
+  isLoading
+  ?(
+    <div className="alert alert-info text-center">Loading...</div>
+
+  ):
+  (
+    <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+  )
+}
+    <button className="btn btn-primary" onClick={increment}>Next quote</button>    
+        
     </>
   )
 }
